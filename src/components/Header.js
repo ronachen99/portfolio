6a2c3f39309f57f8bbaebfe8efb3e7757dd77766
Rona C <rona.chen@hotmail.com>
2023-07-06 12:14:54 -0400

    changed the over color and font theme

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,67 +5,67 @@ import React from "react";
 function Header({ currentPage, handlePageChange }) {
   return (
     // parent div that aligns the the two children div at the start and the end of the container
-    // the first inner div is the title
+    // the first child is the title
     // the second inner div is the nav bar
     // based on the currentPage prop, sets the conditional class active
     // pass the current page as arg to the handlePageChange function
 
-    <div className="flex flex-col sm:flex-row justify-between">
-      <div className="flex m-8 rounded-lg lowercase justify-center text-5xl px-4 py-2 text-indigo-500 bg-gray-300">
-        Rona's Portfolio
-      </div>
+    <div className="flex flex-col items-center sm:flex-row sm:justify-between bg-gray-900">
+      <h1 className="flex m-8 rounded-lg lowercase justify-center text-6xl px-4 py-2 text-green-600 bg-gray-800">
+        rona_s_portfolio
+      </h1>
       <div className="flex m-8">
-        <ul className="flex space-x-4">
+        <ul className="flex space-x-4 text-lg">
           <li>
             <a
               href="#about"
               onClick={() => handlePageChange("About")}
-              className={`nav-link ${
+              className={`${
                 currentPage === "About"
-                  ? "bg-indigo-500 text-white"
-                  : "bg-gray-300 text-indigo-500"
-              } rounded-lg px-4 py-2 hover:bg-indigo-400 hover:text-white`}
+                  ? "bg-green-600 text-white"
+                  : "bg-gray-800 text-green-600"
+              } rounded-lg px-4 py-2 hover:bg-green-500 hover:text-white`}
             >
-              About Me
+              aboutMe
             </a>
           </li>
           <li>
             <a
               href="#portfolio"
               onClick={() => handlePageChange("Portfolio")}
-              className={`nav-link ${
+              className={`${
                 currentPage === "Portfolio"
-                  ? "bg-indigo-500 text-white"
-                  : "bg-gray-300 text-indigo-500"
-              } rounded-lg px-4 py-2 hover:bg-indigo-400 hover:text-white`}
+                  ? "bg-green-600 text-white"
+                  : "bg-gray-800 text-green-600"
+              } rounded-lg px-4 py-2 hover:bg-green-500 hover:text-white`}
             >
-              Portfolio
+              portfolio
             </a>
           </li>
           <li>
             <a
               href="#contact"
               onClick={() => handlePageChange("Contact")}
-              className={`nav-link ${
+              className={`${
                 currentPage === "Contact"
-                  ? "bg-indigo-500 text-white"
-                  : "bg-gray-300 text-indigo-500"
-              } rounded-lg px-4 py-2 hover:bg-indigo-400 hover:text-white`}
+                  ? "bg-green-600 text-white"
+                  : "bg-gray-800 text-green-600"
+              } rounded-lg px-4 py-2 hover:bg-green-500 hover:text-white`}
             >
-              Contact
+              contact
             </a>
           </li>
           <li>
             <a
               href="#resume"
               onClick={() => handlePageChange("Resume")}
-              className={`nav-link ${
+              className={`${
                 currentPage === "Resume"
-                  ? "bg-indigo-500 text-white"
-                  : "bg-gray-300 text-indigo-500"
-              } rounded-lg px-4 py-2 hover:bg-indigo-400 hover:text-white`}
+                  ? "bg-green-600 text-white"
+                  : "bg-gray-800 text-green-600"
+              } rounded-lg px-4 py-2 hover:bg-green-500 hover:text-white`}
             >
-              Resume
+              resume
             </a>
           </li>
         </ul>
