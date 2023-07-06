c882cf1c177dc3fb1adfc10aec8bddac98053ad1
Rona C <rona.chen@hotmail.com>
2023-07-06 00:31:26 -0400

    added app.js with require component and rendering of the pages and added index.js that acts as the root for the app

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import NavTabs from "./components/NavTabs";
+import Footer from "./components/Footer";
+import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
+import Resume from "./pages/Resume";
+
+function App() {
+  const [currentPage, setCurrentPage] = useState("About");
+
+  const renderPage = () => {
+    if (currentPage === "About") {
+      return <About />;
+    }
+    if (currentPage === "Portfolio") {
+      return <Portfolio />;
+    }
+    if (currentPage === "Contact") {
+      return <Contact />;
+    }
+    return <Resume />;
+  };
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
