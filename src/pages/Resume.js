65dd0347b03cb3b146ed5c0db044db33a8ca0a25
Rona C <rona.chen@hotmail.com>
2023-07-06 21:55:31 -0400

    updated resume page

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,36 +1,58 @@
 import React from "react";
+import { SiAdobeacrobatreader } from "react-icons/si";
 
 export default function Resume() {
   return (
-    <div>
-      <h1>Resume</h1>
-      <p>
-        Click on this{" "}
-        <a href="./resume.pdf" download>
-          ⚝
-        </a>{" "}
-        to download my resume!
-      </p>
-      <h2>Front-end Proficiencies</h2>
-      <ul>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>JavaScript</li>
-        <li>JQuery</li>
-        <li>Responsive Design</li>
-        <li>Bootstrap</li>
-        <li>Bulma</li>
-        <li>Animate CSS</li>
-      </ul>
-      <h2>Back-end Proficiencies</h2>
-      <ul>
-        <li>APIs</li>
-        <li>Node.Js</li>
-        <li>Express</li>
-        <li>MySQL, Sequelize</li>
-        <li>MongoDB, MonGoose</li>
-        <li>Handlebars</li>
-      </ul>
+    <div className="flex flex-col m-8 space-y-8 items-center">
+      <h2 className="flex m-8 rounded-lg justify-center text-6xl px-4 py-2 text-green-600 bg-gray-800">
+        resume
+      </h2>
+      <h3 className="flex text-center text-green-600 text-4xl">
+        click on the following icon to download my resume
+        <a href="./resume.pdf" download className="flex items-center ml-2">
+          <SiAdobeacrobatreader className="cursor-pointer" />
+        </a>
+      </h3>
+      <div className="flex flex-row space-x-8">
+        <div className="space-y-8 bg-gray-800 rounded-lg px-4 py-2 text-gray-500">
+          <h3 className="text-green-600 text-4xl">frontend_proficiencies</h3>
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>React</li>
+            <li>JQuery</li>
+            <li>Responsive Design</li>
+            <li>Handlebars</li>
+          </ul>
+        </div>
+        <div className="space-y-8 bg-gray-800 rounded-lg px-4 py-2 text-gray-500">
+          <h3 className="text-green-600 text-4xl">css_libraries</h3>
+          <ul>
+            <li>Bootstrap</li>
+            <li>Bulma</li>
+            <li>Animate CSS</li>
+            <li>Tailwind CSS</li>
+          </ul>
+        </div>
+        <div className="space-y-8 bg-gray-800 rounded-lg px-4 py-2 text-gray-500">
+          <h3 className="text-green-600 text-4xl">backend_proficiencies</h3>
+          <ul>
+            <li>APIs</li>
+            <li>Node.Js</li>
+            <li>Express</li>
+            <li>Apollo Server</li>
+          </ul>
+        </div>
+        <div className="space-y-8 bg-gray-800 rounded-lg px-4 py-2 text-gray-500">
+          <h3 className="text-green-600 text-4xl">databases</h3>
+          <ul>
+            <li>MySQL, Sequelize</li>
+            <li>MongoDB, MonGoose</li>
+            <li>IndexedDB</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
